00410709f47adec051f1f0129b196b11350ad8b0
shubhangi0201 <shubhangi@vincentio.in>
2025-02-06 13:57:13 +0530

    first commit

--- Base/urls.py
+++ Base/urls.py
@@ -3,7 +3,7 @@ from Base import views  # Ensure correct case usage
 
 urlpatterns = [
     
-    path('About/', views.About, name='About'),
+    
     path('Home/', views.Home, name='Home'),
     path('', views.Home, name='Base'),
       # Correct reference to views
